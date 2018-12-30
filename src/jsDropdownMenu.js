e3f6bce3fe515344fb33603faa5afcff21122cfe
Bohdan Partyk <info@podebrady.org>
2018-12-30 15:09:46 +0100

    fix import module. Remove AMD

--- src/jsDropdownMenu.js
+++ src/jsDropdownMenu.js
@@ -1,18 +1,10 @@
-/* global define */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['b'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory(require('b'));
+    if (typeof module === 'object') {
+        module.exports = factory();
     } else {
-        // Browser globals (root is window)
-        root.JsDropdownMenu = factory(root.b);
+        root.greatLibrary = factory();
     }
-}(typeof self !== 'undefined' ? self : this, function (b) {
+}(this, function () {
     class JsDropdownMenu {
         constructor(options) {
             this.options = {
